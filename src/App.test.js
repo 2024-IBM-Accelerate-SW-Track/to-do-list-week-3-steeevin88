3a7c267478ec9780c62cd4babc7154cf6d3c981e
Steven Le <stevenleusa79@yahoo.com>
2024-06-24 00:25:16 -0700

    added tests + passed locally

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
@@ -23,11 +23,100 @@ test('test that App component renders', () => {
 test('test that new-item-button is a button', () => {
   render(<App/>, container);
   const element = screen.getByTestId('new-item-button');
-  expect(element.innerHTML.toLowerCase().includes("button")).toBe(true)
+  expect(element.outerHTML.toLowerCase().includes("button")).toBe(true)
 });
 
 test('test that new-item-input is an input ', () => {
   render(<App/>, container);
   const element = screen.getByTestId('new-item-input');
   expect(element.innerHTML.toLowerCase().includes("input")).toBe(true)
+});
+
+test('test for no duplicate tasks', () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  // add a task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+
+  // add the same task again
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getByText(/History Test/i);                // if more than one value is present, this will fail
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));   // if more than one value is present, this will fail
+  expect(check).toBeInTheDocument();
+  expect(checkDate).toBeInTheDocument();
+});
+
+test('test for no task without due date', () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  // add a task without due date
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.click(element);
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+});
+ 
+test('test for no task without task name', () => {
+  render(<App />);
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+
+  // add a task without task name
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
+});
+
+test('test for last tasks having different colors', () => {
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+
+  const pastDate = "05/30/1900";
+
+  // add the task, now having a due date in the past
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: pastDate }});
+  fireEvent.click(element);
+
+  // check color is not white
+  const colorCheck = screen.getByTestId(/History Test/i).style.background
+  expect(colorCheck).not.toBe("white");
+});
+
+test('test for deleting a task', () => {
+  // first, add a task
+  render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2024";
+
+  // add the task
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate }});
+  fireEvent.click(element);
+
+  // confirm task was added; then, delete the task
+  const check = screen.getByTestId(/History Test/i)
+  expect(check).toBeInTheDocument();
+  fireEvent.click(screen.getByRole('checkbox'));
+
+  // confirm task was deleted --> two checks for fun, both should pass
+  expect(check).not.toBeInTheDocument();
+  const noTodosCheck = screen.getByText(/You have no todo's left/i);
+  expect(noTodosCheck).toBeInTheDocument();
 });
